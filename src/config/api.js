26c4a6e8cd0e61b1f0598e02a4ef08e31924deba
Chirag-skillcrafters <chirag.skillcrafters@gmail.com>
2025-07-27 14:01:54 +0530

    Implement code changes to enhance functionality and improve performance

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
-// export const API_BASE_URL =
-//   // "https://portfolio-backend-uzey.onrender.com/api/v1";
-export const API_BASE_URL = "http://localhost:4000/api/v1";
+export const API_BASE_URL =
+  "https://portfolio-backend-vqki.onrender.com/api/v1";
+// export const API_BASE_URL = "http://localhost:4000/api/v1";
